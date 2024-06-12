c6ff15d9a9e3e184c6fb9f19079690b4d4ebfe9b
amanzo <amanzo@medline.com>
2024-06-12 14:04:27 -0500

    First Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ATA_Analisis
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
